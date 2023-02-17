99671742df6bbe24eea1097a8809e8e31cbb370a
Bayci <baycikplus@gmail.com>
2023-02-17 16:45:19 +0300

    bugfixes???

--- www/firebase-messaging-sw.js
+++ www/firebase-messaging-sw.js
@@ -21,7 +21,7 @@ messaging.onBackgroundMessage(async (payload) => {
   const cl=await clients.matchAll({includeUncontrolled: false, type: 'window'});
   if( cl.length ){
     cl.forEach(client => client.postMessage(payload));
-    //return false;
+    return false;
   }
   
   if(payload.data.body){//if body is set then this is foreground notification!
@@ -30,8 +30,7 @@ messaging.onBackgroundMessage(async (payload) => {
       body: payload.data.body,
       icon: payload.data.icon,
       link: payload.data.link,
-      vibrate: [200, 100, 200],
-      renotify:true
+      vibrate: [200, 100, 200]
     };
     return self.registration.showNotification(notificationTitle,notificationOptions);
   }
